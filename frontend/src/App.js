0a79543368cfcaa7c90c780e21c5944660d4ab68
Valery Kotov <kotov.valery@gmail.com>
2024-05-11 20:20:32 +0200

    [Frontend] Use ChartJS to draw dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,73 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-function App() {
-  const telemetry = [
-    { id: 0, temperature: 22, humidity: 10 },
-    { id: 1, temperature: 23, humidity: 11 },
-    { id: 2, temperature: 24, humidity: 12 },
-    { id: 3, temperature: 20, humidity: 11 },
-    { id: 4, temperature: 21, humidity: 10 },
-  ];
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
 
-  
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Telemetry Data',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const telemetry = [
+  { id: 0, temperature: 22, humidity: 10 },
+  { id: 1, temperature: 23, humidity: 11 },
+  { id: 2, temperature: 24, humidity: 12 },
+  { id: 3, temperature: 20, humidity: 11 },
+  { id: 4, temperature: 21, humidity: 10 },
+  { id: 5, temperature: 22, humidity: 10 },
+  { id: 6, temperature: 23, humidity: 11 },
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Temperature',
+      data: labels.map((_, index) => telemetry[index].temperature),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Humidity',
+      data: labels.map((_, index) => telemetry[index].humidity),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+function App() {
   return (
     <div className="App">
       <h1>Show my telemetry data</h1>
@@ -19,19 +76,13 @@ function App() {
 
       <hr />
 
-      <List list={telemetry} />
+      <div id="TelemetryChart" style={{ width: '100%', height: '100%' }}>
+        <Line options={options} data={data} />
+      </div>
+
 
     </div>
   );
 }
-
-const List = props =>
-  props.list.map((item) => (
-    <div key={item.id}>
-      <h3>Temperature: {item.temperature}</h3>
-      <h3>Humidity: {item.humidity}</h3>
-      <hr />
-    </div>
-  ));
 
 export default App;
