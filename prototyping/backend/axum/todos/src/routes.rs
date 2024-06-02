db771eaf979b305d5d8237b902326280f2cea68e
Valery Kotov <kotov.valery@gmail.com>
2024-06-02 16:53:49 +0200

    [Proto] Return IntoResponse from handlers

--- prototyping/backend/axum/todos/src/routes.rs
+++ prototyping/backend/axum/todos/src/routes.rs
@@ -4,7 +4,7 @@ use crate::state::AppState;
 use std::sync::{Arc, Mutex};
 
 use axum::extract::{Path, State};
-use axum::Json;
+use axum::{http::StatusCode, response::IntoResponse, Json};
 
 pub async fn list_todos(State(state): State<Arc<Mutex<AppState>>>) -> String {
     let state = state.lock().unwrap();
@@ -14,42 +14,53 @@ pub async fn list_todos(State(state): State<Arc<Mutex<AppState>>>) -> String {
     })
 }
 
-pub async fn create_todo(State(state): State<Arc<Mutex<AppState>>>, Json(input): Json<Todo>) -> String {
-    let todo = Todo {
+pub async fn create_todo(
+    State(state): State<Arc<Mutex<AppState>>>,
+    Json(input): Json<Todo>,
+) -> impl IntoResponse {
+    let create = Todo {
         id: input.id,
         description: input.description,
         completed: input.completed,
     };
-    info!("Got a todo item to create: {:?}", todo);
     let mut state = state.lock().unwrap();
-    state.storage.push(todo);
-    "Created".to_string()
+    state.storage.push(create.clone());
+    (StatusCode::CREATED, Json(create))
 }
 
-pub async fn delete_todo(Path(todo_id): Path<u64>, State(state): State<Arc<Mutex<AppState>>>) -> String {
-    info!("Todo id: {}", todo_id);
+pub async fn delete_todo(
+    Path(id): Path<u64>,
+    State(state): State<Arc<Mutex<AppState>>>,
+) -> impl IntoResponse {
     let mut state = state.lock().unwrap();
-    state.storage.retain(|todo| todo.id != todo_id);
-    "Deleted".to_string()
+    let count = state.storage.len();
+    state.storage.retain(|todo| todo.id != id);
+    if state.storage.len() != count {
+        (
+            StatusCode::NO_CONTENT,
+            format!("Item with id {} was deleted", id),
+        )
+    } else {
+        (StatusCode::NOT_FOUND, format!("Did not find {} item", id))
+    }
 }
 
 pub async fn update_todo(
     Path(todo_id): Path<u64>,
     State(state): State<Arc<Mutex<AppState>>>,
     Json(input): Json<Todo>,
-) -> String {
+) -> Result<impl IntoResponse, StatusCode> {
     let update = Todo {
         id: input.id,
         description: input.description,
         completed: input.completed,
     };
-    info!("Todo id {} and content {:?}", todo_id, update);
     let mut state = state.lock().unwrap();
     for todo in state.storage.iter_mut() {
         if todo.id == todo_id {
-            *todo = update;
+            *todo = update.clone();
             break;
         }
     }
-    "Updated".to_string()
+    Ok(Json(update))
 }
