f8faa216b5040fb522938c3155b6345acbe29f98
Valery Kotov <kotov.valery@gmail.com>
2024-06-11 18:27:19 +0200

    [UI] Fetch example data with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import './App.css'
-import React from 'react'
-import TelemetryChart from './components/TelemetryChart'
-import TelemetryData from './components/TelemetryData'
-import { SettngsProvier } from './components/settingsContext'
+import "./App.css";
+import React from "react";
+import TelemetryChart from "./components/TelemetryChart";
+import TelemetryData from "./components/TelemetryData";
+import { SettngsProvier } from "./components/settingsContext";
+import FetchPosts from "./components/FetchPosts";
 
-function App () {
+function App() {
   return (
-    <div className="App" style={{ width: '100vw', height: '100vh' }}>
+    <div className="App" style={{ width: "100vw", height: "100vh" }}>
       <h1>Show my telemetry data</h1>
       <hr />
-      <SettngsProvier value='chart'>
+      <FetchPosts />
+      <hr />
+      <SettngsProvier value="chart">
         <TelemetryData
           render={(labels, telemetry) => (
             <TelemetryChart labels={labels} telemetry={telemetry} />
           )}
         />
       </SettngsProvier>
-
     </div>
-  )
+  );
 }
 
 export default App;
