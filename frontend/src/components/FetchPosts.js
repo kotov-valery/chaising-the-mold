aa0973c99bc8dd3ea298be7cd886a18e515decf8
Valery Kotov <kotov.valery@gmail.com>
2024-06-12 09:58:44 +0200

    [UI] Fetch single post

--- frontend/src/components/FetchPosts.js
+++ frontend/src/components/FetchPosts.js
@@ -2,27 +2,31 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function FetchPosts() {
-  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState({});
+  const [id, setId] = useState(1);
+  const [idFromButtonClick, setIdFromButtonClick] = useState(1);
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
       .then((response) => {
-        setPosts(response.data);
+        setPost(response.data);
       })
       .catch((error) => {
         console.error("Error fetching data: ", error);
       });
-  }, []);
+  }, [idFromButtonClick]);
 
   return (
     <div>
       <h1>Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+      <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
+      <button onClick={() => setIdFromButtonClick(id)}>Fetch Post</button>
+      <p>
+        <strong>Title:</strong> {post.title}
+        <hr />
+        {post.body}
+      </p>
     </div>
   );
 }
